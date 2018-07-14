0c6648c2fe8a3e0bbb86017f483f667e17acb999
ttuzki <916548019@qq.com>
2018-07-14 20:02:03 +0800

    finish charpter model

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 from app import app
-
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World"
+	user = {'nickname':'LJG'}
+	posts = [
+		{
+			'author' : {'nickname':'John'},
+			'body' : 'Beautiful day in Portland!'
+		},
+		{
+			'author':{'nickname':'Bell'},
+			'body':'The Avengers movie was so cool!'
+		}
+	]
+	return render_template("index.html",
+		title = 'Home',
+		user = user,
+		posts = posts)
