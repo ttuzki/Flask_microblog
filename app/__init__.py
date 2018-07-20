4cf541eb2cee31dcabff1d4d45e8c5375261e9e5
ttuzki <916548019@qq.com>
2018-07-20 23:33:24 +0800

    完成用户登陆章节，但未启动成功，run.py可运行，但可能运行方式有误

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+# import sys
+# sys.path.append("..")
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-	
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import *
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
-from app.form import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.form import LoginForm, RegistrationForm
+from app.models import User
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = {'nickname':'LJG'}
 	posts = [
 		{
 			'author' : {'nickname':'John'},
@@ -17,15 +21,41 @@ def index():
 	]
 	return render_template("index.html",
 		title = 'Home',
-		user = user,
 		posts = posts)
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html',
-        title = 'Sign In',
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
